d376dce11296802f2e6586fe8fe0e39f7518f45d
babla45 <134688680+babla45@users.noreply.github.com>
2024-05-03 12:20:10 +0600

    Navigation drawer + website manager + short story all are working perfectly

---
+++ app/src/main/java/com/example/softwareproject3_1/NavigationDrawer.java
@@ -0,0 +1,96 @@
+package com.example.softwareproject3_1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class NavigationDrawer extends AppCompatActivity {
+    DrawerLayout drawerLayout;
+    ImageButton imageButton;
+    NavigationView navigationView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        drawerLayout=findViewById(R.id.main);
+        imageButton=findViewById(R.id.menuButtonId);
+        navigationView=findViewById(R.id.navigationViewId);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.open();
+            }
+        });
+
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int menuId=menuItem.getItemId();
+                if(menuId==R.id.homeMenuId)
+                {
+                    Toast.makeText(NavigationDrawer.this, "Home menu clicked", Toast.LENGTH_SHORT).show();
+                }
+                else if(menuId==R.id.contactMenuId)
+                {
+                    Toast.makeText(NavigationDrawer.this, "Contact menu clicked", Toast.LENGTH_SHORT).show();
+                }
+                else if(menuId==R.id.settingMenuId)
+                {
+                    Toast.makeText(NavigationDrawer.this, "Setting menu clicked", Toast.LENGTH_SHORT).show();
+                }
+                else if(menuId==R.id.shareMenuId)
+                {
+                    Toast.makeText(NavigationDrawer.this, "Share menu clicked", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(NavigationDrawer.this, "NO menu clicked", Toast.LENGTH_SHORT).show();
+                }
+
+                drawerLayout.close();
+                return false;
+            }
+        });
+
+    }
+
+
+    public void website(View view) {
+        Intent intent=new Intent(NavigationDrawer.this, websiteManager.class);
+        if(view.getId()==R.id.youtubeButtonId)
+        {
+            intent.putExtra("youtube","https://www.youtube.com");
+        }
+        if(view.getId()==R.id.googleButtonId)
+        {
+            intent.putExtra("youtube","https://www.google.com");
+        }
+        if(view.getId()==R.id.portfolioButtonId)
+        {
+            intent.putExtra("youtube","https://babla45.github.io/myPortfolio");
+        }
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/softwareproject3_1/websiteManager.java
@@ -0,0 +1,64 @@
+package com.example.softwareproject3_1;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class websiteManager extends AppCompatActivity {
+    private WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_website_manager);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        webView=findViewById(R.id.webViewId);
+
+        WebSettings webSettings=webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        //webSettings.setUserAgentString("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36");
+
+        //enable desktop mode
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setUseWideViewPort(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        //webView.loadUrl("https://www.youtube.com");
+
+        String url="www.google.com";
+        //-----bundle is  for which website button is pressed
+        //data receive from another activity
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null)
+        {
+            String text=bundle.getString("youtube");
+            if (text != null) {
+                url=text;
+            }
+        }
+
+        webView.loadUrl(url);
+    }
+    @Override
+    public void onBackPressed()
+    {
+        if(webView.canGoBack()) webView.goBack();
+        else super.onBackPressed();
+    }
+
+
+
+}
