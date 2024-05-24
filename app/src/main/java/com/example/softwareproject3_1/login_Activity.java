f707c28d94ca9960cecfd00566679b765d357104
Md Babla Islam <bilasislam5643@gmail.com>
2024-05-24 23:04:31 +0600

    fixed night mode theme, layout for sign in and sign up page have now an extra text view to swith between login and registration activity whenever they are clicked

--- app/src/main/java/com/example/softwareproject3_1/login_Activity.java
+++ app/src/main/java/com/example/softwareproject3_1/login_Activity.java
@@ -1,6 +1,12 @@
 package com.example.softwareproject3_1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +14,42 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class login_Activity extends AppCompatActivity {
+public class login_Activity extends AppCompatActivity implements View.OnClickListener {
+    Button loginButton;
+    EditText emailEditText, passwordEditText;
+    TextView setTextTextView, registerTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        loginButton=findViewById(R.id.loginButtonId);
+
+        emailEditText=findViewById(R.id.emailEditTextId);
+        passwordEditText=findViewById(R.id.passwordEditTextId);
+
+        setTextTextView=findViewById(R.id.loginPageSetTextViewId);
+        registerTextView=findViewById(R.id.signUpTextViewId);
+
+        loginButton.setOnClickListener(this);
+        registerTextView.setOnClickListener(this);
+
+    }
+    
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==R.id.signUpTextViewId)
+        {
+            Intent intent=new Intent(login_Activity.this, registration_Activity.class);
+            startActivity(intent);
+        }
+
+        if(v.getId()==R.id.loginButtonId)
+        {
+            Toast.makeText(this, "Login button clicked", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/example/softwareproject3_1/registration_Activity.java
+++ app/src/main/java/com/example/softwareproject3_1/registration_Activity.java
@@ -1,6 +1,12 @@
 package com.example.softwareproject3_1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +14,42 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class registration_Activity extends AppCompatActivity {
+public class registration_Activity extends AppCompatActivity implements View.OnClickListener {
+
+    Button registerButton;
+    TextView registerSetText;
+    TextView registerPageToLoginPage;
+    EditText email,password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_registration);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        registerButton=findViewById(R.id.registerButtonId);
+        registerSetText=findViewById(R.id.registerSetTextViewId);
+        registerPageToLoginPage=findViewById(R.id.loginTextViewId);
+        email=findViewById(R.id.emailEditTextId);
+        password=findViewById(R.id.passwordEditTextId);
+
+        registerButton.setOnClickListener(this);
+        registerPageToLoginPage.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if(v.getId()==R.id.loginTextViewId)
+        {
+            Intent intent=new Intent(registration_Activity.this, login_Activity.class);
+            startActivity(intent);
+        }
+
+        if(v.getId()==R.id.registerButtonId)
+        {
+            Toast.makeText(this, "Register button clicked", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
