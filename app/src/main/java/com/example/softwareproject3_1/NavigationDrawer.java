b31218afe5a4dd26b7201a0e7d4203cdb53b44d2
Md Babla Islam <bilasislam5643@gmail.com>
2024-05-26 02:47:13 +0600

    add line break

--- app/src/main/java/com/example/softwareproject3_1/NavigationDrawer.java
+++ app/src/main/java/com/example/softwareproject3_1/NavigationDrawer.java
@@ -102,6 +102,7 @@ public class NavigationDrawer extends AppCompatActivity {
         if(view.getId()==R.id.portfolioButtonId)
         {
 //            intent.putExtra("youtube","https://babla45.github.io/myPortfolio");
+            //
             startActivity(new Intent(NavigationDrawer.this, blog.class));
         }
         startActivity(intent);
